0915563dbcfa3c1cb8571335de616f75dbe90b48
Matt Brotman <matt.brotman@yahoo.com>
2017-05-23 15:21:20 -0400

    Initial Commit

---
+++ Imageverse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imageverse
+//
+//  Created by Main on 5/23/17.
+//  Copyright © 2017 Matt Brotman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imageverse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imageverse
+//
+//  Created by Main on 5/23/17.
+//  Copyright © 2017 Matt Brotman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
